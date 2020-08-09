12666e3e15351cafc9acdf3dd95c3def4883060b
Mandar1993 <60645059+Mandar1993@users.noreply.github.com>
2020-08-10 02:04:33 +0530

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,3 +1,6 @@
+#2. Write a Python program to accept the user's first and last name and then getting them
+#printed in the the reverse order with a space between first name and last name.
+
 a=input(" Enter First name:")
 b=input(" Enter LAST NAME:")
 print(b+ " " + a)
