81ab3ebe4179d53360541abe3a34781d260378e9
Mandar1993 <60645059+Mandar1993@users.noreply.github.com>
2020-08-10 02:07:15 +0530

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,5 +1,10 @@
+#Write a Python program to find the volume of a sphere with diameter 12 cm.
+
 d=12
 import math
 r=d/2
 v=(4/3)*math.pi*r*r*r
 print("volume of sphere is:",v)
+
+
+#outPut:volume of sphere is: 904.7786842338604
